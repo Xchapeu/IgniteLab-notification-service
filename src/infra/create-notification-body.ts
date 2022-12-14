2d8d10da1f250741e7ef98e9dfe22be869f7f105
xchapeu <xchapeu@gmail.com>
2022-12-14 12:56:25 -0300

    Creating first tests

---
+++ src/infra/create-notification-body.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty, IsUUID, Length } from "class-validator";
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+  
+  @IsNotEmpty()
+  category: string;
+  
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+}
